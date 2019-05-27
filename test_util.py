7aaaa2cbf5548e70ee67a2a9057672b2697b5ff6
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2019-05-26 18:33:31 -0600

    test second get_test_dir()

--- test_util.py
+++ test_util.py
@@ -8,3 +8,8 @@ from util import get_test_dir
 def test_get_test_dir():
     td = get_test_dir()
     assert os.path.isdir(td)
+
+    # second call should also succeed, even though td exists
+    td = get_test_dir()
+    assert os.path.isdir(td)
+    os.rmdir(td)
