f96291e9b468d6e403aafcce41a94caa7a7caa01
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2019-06-01 11:39:07 -0600

    remove debugging code

--- results.py
+++ results.py
@@ -10,8 +10,6 @@ def get_results(mutant_dir, mutant_results):
     with open(mutant_results, 'r') as f:
         for result in f:
             res = result.split()  # mutant, symptom, code
-            print("res[0] is {}".format(res[0]))
             res[0] = get_pos(res[0], root=mutant_dir)
-            print("which turns into {}".format(res[0]))
             rlist.append(res)
     return rlist
